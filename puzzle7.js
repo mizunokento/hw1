260f38f32a39a0aa905b5d94081d37aeb2041039
mizunokento <kmizuno@cca.edu>
2017-01-22 17:02:22 -0800

    Create puzzle7.js

---
+++ puzzle7.js
@@ -0,0 +1,18 @@
+while (getColor()!="red"){
+       down();}
+right();
+right();
+while (getColor()!="red"){
+       up();}
+right();
+right();
+while (getColor()!="red"){
+       down();}
+right();
+right();
+while (getColor()!="red"){
+       up();}
+right();
+right();while (getColor()!="red"){
+       down();}
+right();
