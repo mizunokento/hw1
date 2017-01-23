be13745db2cecb8ecb546e98841757e08e46f9ca
mizunokento <kmizuno@cca.edu>
2017-01-22 17:02:07 -0800

    Create puzzle6.js

---
+++ puzzle6.js
@@ -0,0 +1,24 @@
+right();
+if(getColor()=="blue"){
+   down();
+   down();
+   down();
+  if(getColor()=="blue"){
+    right();
+    down();}
+  else{
+    left();
+    down();
+  }
+}else{
+up();
+up();
+up();}
+if(getColor()=="blue"){
+   right();
+   up();}
+else {
+  left();
+  up();
+}
+
